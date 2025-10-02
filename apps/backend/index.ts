244142303f84ae45535cbba237eb4858fca6e750
Elias-Larsson <aroezi05@gmail.com>
2025-10-02 17:50:41 +0200

    feat: add configuration for auth0 login/logout

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,11 +1,29 @@
 // apps/api/index.ts
 import express from 'express';
+import { auth, requiresAuth } from 'express-openid-connect';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
-  res.send('Hello from Express with Bun in Turborepo!');
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });
 
 app.listen(port, () => {
