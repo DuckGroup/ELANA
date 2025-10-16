c8958c26694588e3e17c87212c40292cbccce05b
obeyhades <abdu.nazar145@gmail.com>
2025-10-16 16:27:13 +0200

    feat: add createUser Service

--- apps/backend/src/services/userService.ts
+++ apps/backend/src/services/userService.ts
@@ -3,17 +3,32 @@ import type { Response, Request } from "express";
 import { prisma } from "../../prisma/prisma";
 
 export const getUsersService = async (req: Request, res: Response) => {
-     try {
-       const users = await prisma.user.findMany();
-       console.log(users)
-       console.log(!users)
-       if (!users) {
-         res.status(400).json("no users found");
-         return;
-       }
-       return users;
-     } catch (error) {
-       console.error("Error fetching user:", error);
-       res.status(400).json({ message: "Error fetching user" });
-     }
-}
+  try {
+    const users = await prisma.user.findMany();
+    console.log(users);
+    console.log(!users);
+    if (!users) {
+      res.status(400).json("no users found");
+      return;
+    }
+    return users;
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(400).json({ message: "Error fetching user" });
+  }
+};
+
+export const createUserService = async (email: string, role: string) => {
+  if (!email)
+    throw new Error("No email", {
+      cause: "400",
+    });
+  const user = await prisma.user.create({
+    data: {
+      email,
+      role: role || "user",
+    },
+  });
+  console.log("User created:", user);
+  return user;
+};
