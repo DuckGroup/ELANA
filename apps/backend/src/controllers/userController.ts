8c7abae877bf8ab5f5180f697e00a64ecd7d3273
obeyhades <abdu.nazar145@gmail.com>
2025-10-17 14:00:58 +0200

    feat: userCrontroller CRUDS

--- apps/backend/src/controllers/userController.ts
+++ apps/backend/src/controllers/userController.ts
@@ -1,7 +1,11 @@
 import type { Response, Request } from "express";
-import { createUserService, getUsersService } from "../services/userService";
+import { createUserService, deleteUserService, getUserByIdService, getUsersService, updateUserService } from "../services/userService";
 import { PrismaClient } from "@prisma/client/extension";
 import { prisma } from "../../prisma/prisma";
+
+interface UserIdParams {
+    id: string;
+  }
 
 export const getUsers = async (req: Request, res: Response): Promise<void> =>{
     try {
@@ -13,12 +17,22 @@ export const getUsers = async (req: Request, res: Response): Promise<void> =>{
     }
 }
 
+export const getUserById = async (req: Request, res: Response): Promise<void> => {
+    try {
+    
+     const user = await getUserByIdService(req.params.id as string)   
+     res.status(200).json(user);
+    } catch (error) {
+        res.status(404).json({ message: (error as Error).message});
+    }
+
+}
+
 export const createUser = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   try {
-    // Validera att email och role finns här
     const user = await createUserService(req.body.email, req.body.role);
     if (!res.headersSent) {
       res.status(201).json(user);
@@ -32,3 +46,21 @@ export const createUser = async (
     res.status(500).json({ message: "Error creating user", error });
   }
 };
+
+export const deleteUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const user = await deleteUserService(req.params.id as string);
+      res.status(200).json({ message: "User deleted", user });
+    } catch (error) {
+      res.status(400).json({ message: (error as Error).message });
+    }
+  };
+
+  export const updateUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const user = await updateUserService(req.params.id as string, req.body);
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(400).json({ message: (error as Error).message });
+    }
+  };
