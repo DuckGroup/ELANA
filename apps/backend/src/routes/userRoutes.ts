b3fe023dd7573709943d7984e8e16d51fe2edb38
obeyhades <abdu.nazar145@gmail.com>
2025-10-17 14:01:37 +0200

    feat: add user routes

--- apps/backend/src/routes/userRoutes.ts
+++ apps/backend/src/routes/userRoutes.ts
@@ -1,9 +1,13 @@
 import {Router } from "express"; 
-import { createUser, getUsers } from "../controllers/userController";
+import { createUser, deleteUser, getUserById, getUsers, updateUser } from "../controllers/userController";
 
 const userRouter = Router();
 
 userRouter.get("/users", getUsers);
+userRouter.get("/users/:id", getUserById)
 userRouter.post("/users", createUser);
+userRouter.delete("/users/:id", deleteUser);
+userRouter.patch("/users/:id", updateUser);
+
 
 export default userRouter;
