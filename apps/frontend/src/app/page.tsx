5078b77a12294fba4b703ef74ea10bc3d0ec6ce7
Elias Larsson <aroezi05@gmail.com>
2025-10-21 14:03:47 +0200

    feat: add playfair font (#59)
    * add playfair font and remove unused fonts
    
    * remove comments in page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -2,10 +2,8 @@ import { auth0 } from "@/lib/auth0";
 import './globals.css';
 
 export default async function Home() {
-  // Fetch the user session
   const session = await auth0.getSession();
 
-  // If no session, show sign-up and login buttons
   if (!session) {
     return (
       <main>
@@ -19,7 +17,6 @@ export default async function Home() {
     );
   }
 
-  // If session exists, show a welcome message and logout button
   return (
     <main>
       <h1>Welcome, {session.user.name}!</h1>
