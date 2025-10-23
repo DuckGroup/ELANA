0ae7759d71d334681a425e4e079bb6fdc10c35af
Elias-Larsson <aroezi05@gmail.com>
2025-10-23 17:11:53 +0200

    add basket validator

--- apps/backend/src/services/basketService.ts
+++ apps/backend/src/services/basketService.ts
@@ -10,11 +10,6 @@ export const createBasketService = async (user_id: string) => {
       },
     });
   } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      if (error.code === "P2002") {
-        throw new Error("User already has a basket");
-      }
-    }
     throw new Error("Failed to create basket");
   }
 };
