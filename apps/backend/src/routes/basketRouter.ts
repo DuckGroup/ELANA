80a6b66d4347595fc04c3278236fe4adaa1074f0
Elias-Larsson <aroezi05@gmail.com>
2025-10-25 23:33:38 +0200

    create basketController

--- apps/backend/src/routes/basketRouter.ts
+++ apps/backend/src/routes/basketRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { requiresAuth } from "express-openid-connect";
-import { connectBasketController } from "../controllers/basketController";
+import { createBasket, getBasketByUserId } from "../controllers/basketController";
 const basketRouter = Router();
 
-basketRouter.post("/", (req, res) => connectBasketController(req, res))
+basketRouter.post("/", createBasket)
+basketRouter.get("/:id", getBasketByUserId)
 
 export default basketRouter;
  
