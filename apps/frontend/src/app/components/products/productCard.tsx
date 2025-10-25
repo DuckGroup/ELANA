effcb8c863f71890e190fd8c3615cb485e8247ff
Elias-Larsson <aroezi05@gmail.com>
2025-10-25 09:00:26 +0200

    create ProductDisplay component, create product folder

---
+++ apps/frontend/src/app/components/products/productCard.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+type Props = {
+  image: string;
+  product_name: string;
+  title: string;
+  price: number;
+  category?: string;
+};
+export const ProductCard = ({
+  image,
+  title,
+  price,
+  // category,
+  product_name,
+}: Props) => {
+  return (
+    <Link href={`/${product_name}`} className="w-40 md:w-56 hover:shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] transition-shadow duration-200">
+      <article className="flex flex-col h-full">
+        <div className="w-full relative">
+        <Image
+          src={image}
+          alt="product image"
+          width={96}
+          height={96}
+          className="w-96"
+          />
+          </div>
+        <div className="flex flex-col justify-between w-full h-full px-2 pt-6 pb-2">
+          <p className="text-sm text-stone-600 uppercase">silver</p>
+          <p>{title}</p>
+          <p className="text-xl font-medium">{price} kr</p>
+        </div>
+      </article>
+    </Link>
+  );
+};
---
+++ apps/frontend/src/app/components/products/productDisplay.tsx
@@ -0,0 +1,26 @@
+import { Product } from "@/types/product";
+import { ProductCard } from "./productCard";
+type Props = {
+  products: Product[];
+};
+export const ProductDisplay = ({
+    products
+}: Props) => {
+  return (
+    <section className="flex flex-wrap p-4 gap-4">
+      {products.length > 0 ? (
+        products.map((product: Product) => (
+          <ProductCard
+            key={product.id}
+            image="/elana_logo.svg"
+            product_name={product.title}
+            title={product.title}
+            price={product.price}
+          />
+        ))
+      ) : (
+        <p>No products listed, come back next time!</p>
+      )}
+    </section>
+  );
+};
