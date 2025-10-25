76d5200cf52e65c467faffc9869145a0935506c8
Elias-Larsson <aroezi05@gmail.com>
2025-10-25 09:01:03 +0200

    fix header icon size

--- apps/frontend/src/app/components/header.tsx
+++ apps/frontend/src/app/components/header.tsx
@@ -5,12 +5,12 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <nav className="flex justify-between px-4 py-6 w-full items-center">
-      <IconButton size="lg" href="#" icon={User}></IconButton>
+    <nav className="flex justify-between px-4 py-2 w-full items-center">
+      <IconButton size="md" href="#" icon={User}></IconButton>
       <Link href="/">
       <Image src="/elana_logo.svg" alt="logo" width="163" height="80"></Image>
       </Link>
-      <IconButton size="lg" href="#" icon={ShoppingCart}></IconButton>
+      <IconButton size="md" href="#" icon={ShoppingCart}></IconButton>
     </nav>
   );
 };
