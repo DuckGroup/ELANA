a4ae1f30f46ffa4d832c291d2dea7ef6e99d330b
Elias-Larsson <aroezi05@gmail.com>
2025-10-25 23:33:38 +0200

    create basketController

--- apps/backend/src/controllers/basketController.ts
+++ apps/backend/src/controllers/basketController.ts
@@ -1,20 +1,71 @@
 import type { Request, Response } from "express";
-import { publishToQueue } from "../repository/rabbitmq";
-import { createBasketService } from "../services/basketService";
 import amqp from "amqplib";
+import { publishToQueue } from "../queues/connection";
+import { getBasketByUserIdService } from "../services/basketService";
 
 let channel: amqp.Channel | null = null;
 const QUEUE_NAME = "basket_queue";
 
-export const connectBasketController = async (req: Request, res: Response) => {
+export const createBasket = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
+    const { user_id } = req.body;
+    const basket = publishToQueue({
+      event: "create.basket",
+      data: user_id,
+    });
 
-    publishToQueue({ event: "create.basket", data: "68f22931add31f77edfa067d" });
-    res.json();
-
-    return 
+    res.status(200).json({
+      success: true,
+      message: "Basket created successfully",
+    });
+    return;
   } catch (error) {
     console.error("Basket queue connection error:", error);
-    return
+    return;
+  }
+};
+
+export const getBasketByUserId = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const user_id = id;
+
+    if (!user_id || user_id.trim() === "") {
+      res.status(400).json({
+        success: false,
+        message: "Failed to fetch, Missing params",
+      });
+      return;
+    }
+
+    const basket = await getBasketByUserIdService(user_id);
+
+    if (!basket) {
+      res.status(404).json({
+        success: false,
+        message: "Basket not found for this user",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      data: basket,
+      message: "Basket fetched successfully",
+    });
+    return;
+  } catch (error) {
+    console.error("Basket fetch error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch basket",
+    });
+    return;
   }
 };
