63b16039158732a1b166a034cf676308bf5d6dc4
Elias Larsson <aroezi05@gmail.com>
2025-10-27 14:18:19 +0100

    Feat: Add cors (#77)

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -5,16 +5,31 @@ import dotenv from "dotenv";
 import { connectRabbitMQ } from "./src/repository/rabbitmq";
 import productRouter from "./src/routes/productRoutes";
 import userRouter from "./src/routes/userRoutes";
-import { authRouter } from "./src/routes/authRoutes";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
 const app = express();
 const port = parseInt(process.env.PORT || "3013", 10);
+const ALLOWED = process.env.ALLOWED_ORIGIN!;
 const host = "0.0.0.0";
+
 app.use(express.json());
 connectRabbitMQ();
 
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      const allowedList = ALLOWED.split(",").map((s) => s.trim());
+      if (ALLOWED === "*" || allowedList.includes(origin))
+        return callback(null, true);
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+    exposedHeaders: ["Content-Range", "X-Total-Count"],
+  })
+);
 const config = {
   authRequired: false,
   auth0Logout: true,
