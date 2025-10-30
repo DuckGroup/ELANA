3a793200587397afb3f9b7ae035c8147bc275bb9
Elias-Larsson <aroezi05@gmail.com>
2025-10-30 12:56:04 +0100

    save

--- apps/backend/src/routes/basketRouter.ts
+++ apps/backend/src/routes/basketRouter.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { addProductToBasket, createBasket, deleteBasket, getBasketByUserId, removeProductToBasket } from "../controllers/basketController";
 const basketRouter = Router();
 
-basketRouter.post("/", createBasket)
+basketRouter.post("/:id", createBasket)
 basketRouter.post("/add-product", addProductToBasket)
 basketRouter.get("/:id", getBasketByUserId)
 basketRouter.delete("/remove-product", removeProductToBasket)
